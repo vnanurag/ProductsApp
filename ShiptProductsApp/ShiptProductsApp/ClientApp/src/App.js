32d99404bb6c60ff1be93d86bb44b617407555f9
Anurag Vutukuri <Anurag.Vturi@gmail.com>
2019-11-21 18:47:41 -0600

    Removing unwanted Front end files

--- ShiptProductsApp/ShiptProductsApp/ClientApp/src/App.js
+++ ShiptProductsApp/ShiptProductsApp/ClientApp/src/App.js
@@ -1,16 +1,10 @@
 ﻿import React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
 import Products from '../src/components/Products';
 
 export default () => (
   <Layout>
-    <Route exact path='/Home' component={Home} />
-    <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-        <Route path='/' component={Products} />
+    <Route path='/' component={Products} />
   </Layout>
 );
--- ShiptProductsApp/ShiptProductsApp/ClientApp/src/components/NavMenu.js
+++ ShiptProductsApp/ShiptProductsApp/ClientApp/src/components/NavMenu.js
@@ -14,21 +14,6 @@ export default props => (
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav>
-        <LinkContainer to={'/Home'} exact>
-          <NavItem>
-            <Glyphicon glyph='home' /> Home
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/counter'}>
-          <NavItem>
-            <Glyphicon glyph='education' /> Counter
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/fetchdata'}>
-          <NavItem>
-            <Glyphicon glyph='th-list' /> Fetch data
-          </NavItem>
-        </LinkContainer>
         <LinkContainer to={'/'}>
             <NavItem>
                 <Glyphicon glyph='th-list' /> Products
